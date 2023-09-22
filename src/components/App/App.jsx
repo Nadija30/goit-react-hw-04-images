34c96519b8caf092f2498ce251ef00e5b715cd13
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-09-22 17:03:00 +0300

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { ToastContainer } from 'react-toastify';
 import { searchPhoto } from '../ImageInfo/image';
@@ -9,104 +9,94 @@ import { Modal } from '../Modal/Modal';
 import { Loader } from '../Loader/Loader';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    isloading: false,
-    photos: [],
-    photoName: '',
-    page: 1,
-    btnLoadMore: false,
-    selectedPhoto: null,
-  };
+export const App = () => {
+  const { isloading, setIsloading } = useState(false);
+  const { photos, setPhotos } = useState([]);
+  const { photoName, setPhotoName } = useState('');
+  const { page, setPage } = useState(1);
+  const { btnLoadMore, setBtnLoadMore } = useState(false);
+  const { selectedPhoto, setSelectedPhoto } = useState(null);
 
-  componentDidUpdate(_, prevState) {
-    const { photoName, page } = this.state;
-    if (photoName !== prevState.photoName || page !== prevState.page) {
-      this.setState({ isloading: true });
-
-      searchPhoto(photoName, page)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(response.status);
-          }
-          return response.json();
-        })
-        .then(data => {
-          if (data.totalHits === 0) {
-            toast.warning(
-              'Вибачте, немає зображень, які відповідають вашому пошуковому запиту. Будь ласка спробуйте ще раз.'
-            );
-            return;
-          }
-          const totalPage = Math.ceil(data.totalHits / 12);
-
-          if (totalPage > page) {
-            this.setState({ btnLoadMore: true });
-          } else {
-            toast.info('Вибачте, але ви досягли кінця результатів пошуку.');
-            this.setState({ btnLoadMore: false });
-          }
-          const arrPhotos = data.hits.map(
-            ({ id, webformatURL, largeImageURL, tags }) => ({
-              id,
-              webformatURL,
-              largeImageURL,
-              tags,
-            })
-          );
-
-          this.setState(prevState => ({
-            photos: [...prevState.photos, ...arrPhotos],
-          }));
-        })
-        .catch(error => {
-          console.log(error);
-          return toast.error(
-            'Щось пішло не так. Будь-ласка спробуйте пізніше.'
-          );
-        })
-        .finally(() => {
-          this.setState({ isloading: false });
-        });
+  useEffect(() => {
+    if (!photoName) {
+      return;
     }
-  }
+    setIsloading(true);
+    searchPhoto(photoName, page)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.totalHits === 0) {
+          toast.warning(
+            'Вибачте, немає зображень, які відповідають вашому пошуковому запиту. Будь ласка спробуйте ще раз.'
+          );
+          return;
+        }
+        const totalPage = Math.ceil(data.totalHits / 12);
 
-  onSubmitForm = value => {
-    if (value === this.state.photoName) {
+        if (totalPage > page) {
+          setBtnLoadMore(true);
+        } else {
+          toast.info('Вибачте, але ви досягли кінця результатів пошуку.');
+          setBtnLoadMore(false);
+        }
+        const arrPhotos = data.hits.map(
+          ({ id, webformatURL, largeImageURL, tags }) => ({
+            id,
+            webformatURL,
+            largeImageURL,
+            tags,
+          })
+        );
+
+        setPhotos(prevPhotos => [...prevPhotos, ...arrPhotos]);
+      })
+      .catch(error => {
+        console.log(error);
+        return toast.error('Щось пішло не так. Будь-ласка спробуйте пізніше.');
+      })
+      .finally(() => {
+        setIsloading(false);
+      });
+  }, [photoName, page, setBtnLoadMore, setIsloading, setPhotos]);
+
+  const onSubmitForm = value => {
+    if (value === photoName) {
       toast.info('Будь ласка, введіть новий запит!');
       return;
     }
-    this.setState({
-      photoName: value,
-      page: 1,
-      photos: [],
-      btnLoadMore: false,
-    });
+    setPhotoName(value);
+    setPage(1);
+    setPhotos([]);
+    setBtnLoadMore(false);
   };
 
-  onClickRender = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const onClickRender = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  toggleModal = (selectedPhoto = null) => {
-    this.setState({ selectedPhoto });
+  const toggleModal = (selectedPhoto = null) => {
+    setSelectedPhoto(selectedPhoto);
   };
-  render() {
-    const { isloading, photos, btnLoadMore, selectedPhoto } = this.state;
-    return (
-      <div className={css.app}>
-        <Searchbar onSubmit={this.onSubmitForm} />
-        {isloading && <Loader />}
-        <ImageGalery photos={photos} onClickImageItem={this.toggleModal} />
-        {photos.length !== 0 && btnLoadMore && (
-          <Button onClickRender={this.onClickRender} />
-        )}
-        {selectedPhoto && (
-          <Modal selectedPhoto={selectedPhoto} onClose={this.toggleModal} />
-        )}
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className={css.app}>
+      <Searchbar onSubmit={onSubmitForm} />
+      {isloading && <Loader />}
+      <ImageGalery photos={photos} onClickImageItem={toggleModal} />
+      {photos.length !== 0 && btnLoadMore && (
+        <Button onClickRender={onClickRender} />
+      )}
+      {selectedPhoto && (
+        <Modal selectedPhoto={selectedPhoto} onClose={toggleModal} />
+      )}
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
+};
